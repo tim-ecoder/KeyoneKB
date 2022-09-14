b6a5c686785d8245be48c7cc1f5e5c895e61f0e7
tim-ecoder <timafyozzi@yandex.ru>
2022-09-15 00:00:27 +0300

    BUGFIX: Не отбивался вызов

--- app/src/main/java/com/sateda/keyonekb2/input/CallStateCallback.java
+++ app/src/main/java/com/sateda/keyonekb2/input/CallStateCallback.java
@@ -13,7 +13,14 @@ public class CallStateCallback extends PhoneStateListener {
     public void onCallStateChanged(int state, String incomingNumber) {
 
         Log.d(TAG2, "onCallStateChanged state = "+state);
-        calling = state == TelephonyManager.CALL_STATE_RINGING || state == TelephonyManager.CALL_STATE_OFFHOOK;
+        if(state == TelephonyManager.CALL_STATE_IDLE)
+            calling = false;
+        else
+            calling = true;
+        //if(state == TelephonyManager.CALL_STATE_RINGING)
+        //else if(state == TelephonyManager.CALL_STATE_OFFHOOK)
+        //    calling = true;
+
     }
 
     public boolean isCalling() {
